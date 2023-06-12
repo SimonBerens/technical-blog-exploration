57bee540b3b5476a32c10c37ca33e8df04601297
sberens <sberensnyc@gmail.com>
2023-06-11 23:06:48 -0700

    get vitepress katex working

--- vitepress/.vitepress/config.ts
+++ vitepress/.vitepress/config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vitepress'
-
+import {katex} from '@mdit/plugin-katex'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "blog",
   description: "A VitePress Site",
+  markdown: {
+    config: md => {
+      md.use(katex)
+    }
+  }
 })
--- vitepress/.vitepress/theme/Layout.vue
+++ vitepress/.vitepress/theme/Layout.vue
@@ -6,6 +6,8 @@ const { site, frontmatter } = useData()
 </script>
 
 <template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.css" integrity="sha384-3UiQGuEI4TTMaFmGIZumfRPtfKQ3trwQE2JgosJxCnGmQpL/lJdjpcHkaaFwHlcI" crossorigin="anonymous">
+
   <div v-if="frontmatter.home">
     <h1>{{ site.title }}</h1>
     <p>{{ site.description }}</p>
