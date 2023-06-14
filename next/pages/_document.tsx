4e006a0c1a15eda716e0cb3ebfde4c7c3b8bc4a3
sberens <sberensnyc@gmail.com>
2023-06-13 20:10:17 -0400

    get next katex working

--- next/pages/_document.tsx
+++ next/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.css"
+          integrity="sha384-3UiQGuEI4TTMaFmGIZumfRPtfKQ3trwQE2JgosJxCnGmQpL/lJdjpcHkaaFwHlcI"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
